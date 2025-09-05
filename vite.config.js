ee431f753e9b5044d8a3cff284ed4b5ebeabc62e
Nick Kelly <nekelly12@gmail.com>
2025-09-05 18:59:38 -0500

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Spotify-Project/',   // 👈 important for project pages
 })
